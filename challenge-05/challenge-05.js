094ac4f9dc2735a66a02bbc8a95dbf190f156b9a
Pedro Barão <phbarao@gmail.com`>
2020-06-02 15:43:26 -0300

    adding challenge-05 incomplete

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,12 +2,14 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var array = [1, 7, 60, 78, 125, 205];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function myFunction( array ) {
+    return array;
+};
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
